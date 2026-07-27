96fe828da8871c46bf17b283b7f071069f4bbd5a
LeonidGlu <ale.glushckow@yandex.ru>
2026-07-28 03:53:58 +0700

    feat: implement matrix multiplication (matmul) for Tensor class

--- include/tensor/Tensor.h
+++ include/tensor/Tensor.h
@@ -5,37 +5,38 @@
 class Tensor
 {
 public:
-    Tensor(const std::vector<size_t>& shape);
-    Tensor(const std::vector<size_t>& shape, const std::vector<float> data);
-    Tensor(const Tensor& other);
-    Tensor(Tensor&& other) noexcept;
+    Tensor(const std::vector<size_t> &shape);
+    Tensor(const std::vector<size_t> &shape, const std::vector<float> data);
+    Tensor(const Tensor &other);
+    Tensor(Tensor &&other) noexcept;
     ~Tensor() = default;
 
-    float& operator()(const std::vector<size_t>& indices);
-    const float& operator()(const std::vector<size_t>& indices) const;
-    Tensor& operator=(const Tensor& other);
-    Tensor& operator=(Tensor&& other) noexcept;
-    Tensor operator+(const Tensor& other) const;
+    float &operator()(const std::vector<size_t> &indices);
+    const float &operator()(const std::vector<size_t> &indices) const;
+    Tensor &operator=(const Tensor &other);
+    Tensor &operator=(Tensor &&other) noexcept;
+    Tensor operator+(const Tensor &other) const;
     Tensor operator+(const float scalar) const;
-    Tensor operator-(const Tensor& other) const;
+    Tensor operator-(const Tensor &other) const;
     Tensor operator-(const float scalar) const;
     Tensor operator*(const Tensor& other) const;
     Tensor operator*(const float scalar) const;
     Tensor operator/(const Tensor& other) const;
     Tensor operator/(const float scalar) const;
 
+    Tensor matmul(const Tensor& other) const;
+
     void print() const;
     size_t size() const;
-    const std::vector<size_t>& shape() const;
+    const std::vector<size_t> &shape() const;
 
 private:
     void initStrides();
-    void printRecursive(size_t dim, std::vector<size_t>& indices, std::ostream& os, size_t indent) const;
-    void printIndent(std::ostream& os, size_t indent) const; 
-    
+    void printRecursive(size_t dim, std::vector<size_t> &indices, std::ostream &os, size_t indent) const;
+    void printIndent(std::ostream &os, size_t indent) const;
+
     std::vector<float> data_;
     std::vector<size_t> shape_;
     std::vector<size_t> strides_;
     size_t size_;
-
 };
--- src/tensor/Tensor.cpp
+++ src/tensor/Tensor.cpp
@@ -147,7 +147,7 @@ Tensor Tensor::operator-(const float scalar) const {
 
 Tensor Tensor::operator*(const Tensor& other) const {
     if (other.shape_ != shape_) {
-        throw std::runtime_error("The dimensions must be match <operator->");
+        throw std::runtime_error("The dimensions must be match <operator*>");
     }
     std::vector<float> res(size_);
     for (size_t i = 0; i < size_; ++i) {
@@ -188,6 +188,30 @@ Tensor Tensor::operator/(const float scalar) const {
     }
     return Tensor(shape_, res);
 }
+
+Tensor Tensor::matmul(const Tensor& other) const {
+    size_t size = shape_.size();
+    size_t otherSize = other.shape_.size();
+    if (shape_[size - 1] != other.shape_[otherSize - 2]) {
+        throw std::runtime_error("The number of columns and rows must match for multiplication <matmul>");
+    }
+
+    std::vector<float> res(shape_[size - 2] * other.shape_[size - 1]);
+    float sum = 0.0f;
+    size_t index = 0;
+    for (size_t row = 0; row < shape_[size - 2]; ++row) {
+        for (size_t col = 0; col < other.shape_[size - 1]; ++col) {
+            sum = 0.0f;
+            for (size_t i = 0; i < shape_[size - 1]; ++ i) {
+                sum += data_[row * strides_[size - 2] + i * strides_[size - 1]] * other.data_[i * other.strides_[size - 2] + col * other.strides_[size -1]];  
+            }
+            res[index++] = sum;
+        }
+    }
+
+    return Tensor({shape_[size - 2], other.shape_[size - 1]}, res);
+}
+
 
 void Tensor::print() const {
     std::vector<size_t> indices(shape_.size(), 0);
