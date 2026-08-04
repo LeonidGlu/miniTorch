da252c0551de4330137ad64c62258d1988609ee7
LeonidGlu <ale.glushckow@yandex.ru>
2026-08-04 17:50:17 +0700

    test: added parameterized tests with fixture for compound operations for Tensor class

--- tests/test_tensor.cpp
+++ tests/test_tensor.cpp
@@ -30,7 +30,7 @@ Tensor random_tensor(const std::vector<size_t>& shape) {
     return Tensor(shape, data);
 }
 
-std::vector<BinaryOpTestParams> generate_data(size_t count, Tensor (*op)(const Tensor&, const Tensor&), const std::string& opName) {
+std::vector<BinaryOpTestParams> generateDataForBinaryOp(size_t count, Tensor (*op)(const Tensor&, const Tensor&), const std::string& opName) {
     std::vector<BinaryOpTestParams> test_cases;
 
     for (size_t i = 0; i < count; ++i) {
@@ -78,25 +78,108 @@ TEST_P(BinaryOpTest, BinaryOperations){
 INSTANTIATE_TEST_SUITE_P(
     Addition,
     BinaryOpTest,
-    ::testing::ValuesIn(generate_data(100, add_op, "addition"))
+    ::testing::ValuesIn(generateDataForBinaryOp(100, add_op, "addition"))
 );
 
 INSTANTIATE_TEST_SUITE_P(
     Subtraction,
     BinaryOpTest,
-    ::testing::ValuesIn(generate_data(100, sub_op, "subtraction"))
+    ::testing::ValuesIn(generateDataForBinaryOp(100, sub_op, "subtraction"))
 );
 
 INSTANTIATE_TEST_SUITE_P(
     Multiplication,
     BinaryOpTest,
-    ::testing::ValuesIn(generate_data(100, mul_op, "multiplication"))
+    ::testing::ValuesIn(generateDataForBinaryOp(100, mul_op, "multiplication"))
 );
 
 INSTANTIATE_TEST_SUITE_P(
     Division,
     BinaryOpTest,
-    ::testing::ValuesIn(generate_data(100, div_op, "division"))
+    ::testing::ValuesIn(generateDataForBinaryOp(100, div_op, "division"))
+);
+
+struct CompoundOpTestParams{
+    std::string name;
+    std::shared_ptr<Tensor> a;
+    std::shared_ptr<Tensor> b;
+    std::vector<float> expected;
+    void (*op)(Tensor&, const Tensor&);
+};
+
+void add_assign_op(Tensor& x, const Tensor& y) { x += y; };
+void sub_assign_op(Tensor& x, const Tensor& y) { x -= y; };
+void mul_assign_op(Tensor& x, const Tensor& y) { x *= y; };
+void div_assign_op(Tensor& x, const Tensor& y) { x /= y; };
+
+std::vector<CompoundOpTestParams> generateDataForCompoundOp(size_t count, void (*op)(Tensor&, const Tensor&), const std::string& opName) {
+    std::vector<CompoundOpTestParams> test_cases;
+
+    for (size_t i = 0; i < count; ++i) {
+        auto a = random_tensor({2, 2});
+        auto b = random_tensor({2, 2});
+        Tensor copyA = a;
+        op(a, b);
+        std::vector<float> expected(a.size());
+        for (size_t j = 0; j < a.size(); ++j) {
+            expected[j] = a.data()[j];
+        }
+
+        test_cases.push_back({opName + "_" + std::to_string(i), std::make_shared<Tensor>(copyA), std::make_shared<Tensor>(b), expected, op});
+    }
+
+    return test_cases;
+}
+
+class CompoundOpTest : public ::testing::TestWithParam<CompoundOpTestParams> {
+public:
+    CompoundOpTest() = default;
+
+protected:
+    void SetUp() override {
+        const auto& params = GetParam();
+        a = params.a;
+        b = params.b;
+        expected = params.expected;
+        op = params.op;
+    }
+
+    std::shared_ptr<Tensor> a;
+    std::shared_ptr<Tensor> b;
+    std::vector<float> expected;
+    void (*op)(Tensor&, const Tensor&);
+};
+
+TEST_P(CompoundOpTest, CompoundOperations) {
+    op(*a, *b);
+    Tensor result = *a;
+    for (size_t i = 0; i < a->size(); ++i) {
+        EXPECT_FLOAT_EQ(result.data()[i], expected[i]) << "Failed for: " << GetParam().name;
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    CompoundAdd,
+    CompoundOpTest,
+    ::testing::ValuesIn(generateDataForCompoundOp(100, add_assign_op, "CompoundAdd"))
+);
+
+INSTANTIATE_TEST_SUITE_P(
+    CompoundSub,
+    CompoundOpTest,
+    ::testing::ValuesIn(generateDataForCompoundOp(100, sub_assign_op, "CompoundSub"))
+);
+
+INSTANTIATE_TEST_SUITE_P(
+    CompoundMul,
+    CompoundOpTest,
+    ::testing::ValuesIn(generateDataForCompoundOp(100, mul_assign_op, "CompoundMul"))
+);
+
+INSTANTIATE_TEST_SUITE_P(
+    CompoundDiv,
+    CompoundOpTest,
+    ::testing::ValuesIn(generateDataForCompoundOp(100, div_assign_op, "CompoundDiv"))
 );
 
 TEST(TensorTest, Creation1){
